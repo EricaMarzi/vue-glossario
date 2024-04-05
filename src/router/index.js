50034013523e14f8e4f8c4aaae164d6d8bdc370f
Amarildo <you@example.com>
2024-04-05 18:03:26 +0200

    change path homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomePage from '../pages/HomePage.vue';
-import WordDetailPage from '../pages/WordDetailPage.vue';
-import AboutUsPage from '../pages/AboutUsPage.vue';
-import NotFoundPage from '../pages/NotFoundPage.vue';
-
+import HomePage from "../pages/HomePage.vue";
+import WordDetailPage from "../pages/WordDetailPage.vue";
+import AboutUsPage from "../pages/AboutUsPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    linkExatActiveClass: 'active',
-    routes: [
-        { path: '/home', component: HomePage, name: 'home' },
-        { path: '/about-us', component: AboutUsPage, name: 'about-us' },
-        { path: '/words/:slug', component: WordDetailPage, name: 'word-detail' },
-        { path: '/not-found', component: NotFoundPage, name: 'not-found' },
-        { path: '/:pathMatch(.*)*', redirect: '/not-found' }
-    ]
+  history: createWebHistory(),
+  linkExatActiveClass: "active",
+  routes: [
+    { path: "/", component: HomePage, name: "home" },
+    { path: "/about-us", component: AboutUsPage, name: "about-us" },
+    { path: "/words/:slug", component: WordDetailPage, name: "word-detail" },
+    { path: "/not-found", component: NotFoundPage, name: "not-found" },
+    { path: "/:pathMatch(.*)*", redirect: "/not-found" },
+  ],
 });
 
-export { router }
+export { router };
