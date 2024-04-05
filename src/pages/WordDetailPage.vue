ba125e8354dc2029b0739514b11460961f9a245e
DaniAm94 <dani.amico94@gmail.com>
2024-04-05 20:28:45 +0200

    updated WordCard and WordDetailPage

--- src/pages/WordDetailPage.vue
+++ src/pages/WordDetailPage.vue
@@ -1,11 +1,33 @@
 <script>
+import WordCard from '../components/words/WordCard.vue';
+import axios from 'axios';
+const defaultEndpoint = 'http://localhost:8000/api/words/';
 export default {
-    name: 'WordDetailPage'
+    name: 'WordDetailPage',
+    components: { WordCard },
+    data: () => ({
+        word: null
+    }),
+    methods: {
+        async getWord() {
+            try {
+                const res = await axios.get(defaultEndpoint + this.$route.params.slug);
+                this.word = res.data;
+            } catch (err) {
+                console.error(err);
+                this.$router.push({ name: 'not-found' });
+            }
+        }
+    },
+    created() {
+        this.getWord();
+    }
 }
 </script>
 
 <template>
-    word
+    <h1 v-if="word" class="mb-5">Termine: {{ word.term }}</h1>
+    <WordCard :word="word" />
 </template>
 
 <style lang='scss' scoped>
