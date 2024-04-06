5e4c6d893f58e8f2a2e624b2c5bf7c5e2307b2bb
EricaMarzi <erica.marzi.1993@gmail.com>
2024-04-06 13:35:26 +0200

    Home page style

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import { store } from '../data/store';
+import { RouterLink } from 'vue-router';
 const endpoint = 'http://localhost:8000/api/words/';
 
 export default {
@@ -53,33 +54,85 @@ export default {
 </script>
 
 <template>
-    <h1 class="mt-4">Glossario</h1>
+    <h1 class="my-5 text-center title">Il Glossario del Piccolo Sviluppatore</h1>
 
+    <!-- lista delle lettere -->
+    <div class="mt-3">
+        <ul class="pagination justify-content-center d-flex flex-wrap">
+            <li class="page-item" v-for="letter in letters" :key="letter">
+                <a class="page-link alphabet" role="button" @click="filterByLetter(letter)">{{ letter }}</a>
+            </li>
+            <li>
+                <!-- button show all -->
+                <button class="btn btn-custom ms-3" @click="showAllWords">All</button>
+            </li>
+        </ul>
+    </div>
     <div v-if="!store.isLoading && words">
         <!-- lista parole -->
-        <ul class="list-group mt-4">
-            <li class="list-group-item" v-for="word in filteredWords" :key="word.id">
-                {{ word.term }}
+
+        <ul class="grid-container p-5">
+            <li v-for="word in filteredWords" :key="word.id">
+                ➢ <RouterLink :to="`/words/${word.slug}`" class="term">
+                    {{ word.term }}
+                </RouterLink>
             </li>
         </ul>
 
-        <!-- lista delle lettere -->
-        <div class="mt-3">
-            <ul class="pagination justify-content-center d-flex flex-wrap">
-                <li class="page-item" v-for="letter in letters" :key="letter">
-                    <a class="page-link" role="button" @click="filterByLetter(letter)">{{ letter }}</a>
-                </li>
-            </ul>
-        </div>
 
 
-        <!-- button show all -->
-        <button class="btn btn-primary mt-4" @click="showAllWords">All</button>
+
 
     </div>
 
 </template>
 
 <style lang='scss' scoped>
-/* style here */
+.title {
+    color: #105781;
+}
+
+.alphabet {
+    color: #a72c23;
+    font-weight: 600;
+
+    border-color: #c2f0f8;
+
+    &:hover {
+        background-color: #def9fe;
+        ;
+    }
+}
+
+.btn-custom {
+    background-color: #a72c23;
+    color: white;
+    font-weight: 600;
+}
+
+ul {
+    list-style-type: none;
+
+}
+
+
+.grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+
+    .term {
+        text-transform: capitalize;
+        text-decoration: none;
+        color: #00273e;
+        font-size: 18px;
+        font-weight: 600;
+
+        &:hover {
+            color: #a72c23;
+        }
+    }
+
+
+}
 </style>
