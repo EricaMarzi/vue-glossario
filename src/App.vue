91fb3212a81469569068ebff66f418b360bf455d
giamaf <gianluca.maffucci@hotmail.com>
2024-04-06 15:18:59 +0200

    added HomePage and AppSearch function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
-const endpoint = 'http://localhost:8000/api/words/';
 
 export default {
   name: 'Glossario',
   components: { AppHeader },
-  data: () => ({
-    words: []
-  }),
 
-
-  methods: {
-    fetchWords() {
-      axios.get(endpoint).then(res => {
-        console.log(res.data);
-
-        this.words = res.data;
-      })
-    }
-  },
-  created() {
-    this.fetchWords();
-  }
 }
 </script>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,49 @@
 <script>
+import axios from 'axios';
+import AppSearch from '../components/AppSearch.vue';
+import store from '../assets/store';
+const endpoint = 'http://localhost:8000/api/words/';
+
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    data: () => ({
+        store,
+        searchedText: '',
+        placeholder: 'Cerca termine...'
+    }),
+    components: { AppSearch },
+    computed: {
+        // Funzione per filtrare i tasks nella barra di ricerca 
+        // filteredWords() {
+        //     const text = this.searchedText.toLowerCase();
+        //     const words = this.store.words;
+        //     console.log(this.store.words);
+        //     return words.filter(word => word.term.toLowerCase().includes(text));
+        // },
+    },
+    methods: {
+        // Funzione per effettuare la chiamata API
+        fetchWords() {
+            axios.get(endpoint)
+                .then(res => {
+                    store.words = res.data.words;
+                    console.log(store.words)
+                })
+                .catch((err) => {
+                    console.err(err);
+                })
+                .then()
+        },
+
+    },
+    created() {
+        this.fetchWords();
+    }
 }
 </script>
 
 <template>
-
+    <AppSearch :placeholder="placeholder" />
     <h1>Glossario</h1>
     <!-- <ul>
             <li v-for="word in words" :key="word.id" v-text="word.term"></li>
