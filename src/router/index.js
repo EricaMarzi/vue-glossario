f59ada9b720040601a076b0696eb1a0533fffce6
giamaf <gianluca.maffucci@hotmail.com>
2024-04-08 12:05:16 +0200

    added TagWords route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../pages/HomePage.vue";
 import WordDetailPage from "../pages/WordDetailPage.vue";
+import TagWordsPage from "../pages/TagWordsPage.vue"
 import AboutUsPage from "../pages/AboutUsPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
@@ -12,6 +13,7 @@ const router = createRouter({
         { path: '/', component: HomePage, name: 'home' },
         { path: '/about-us', component: AboutUsPage, name: 'about-us' },
         { path: '/words/:slug', component: WordDetailPage, name: 'word-detail' },
+        { path: '/tags/:slug/words', component: TagWordsPage, name: 'tag-words' },
         { path: '/not-found', component: NotFoundPage, name: 'not-found' },
         { path: '/:pathMatch(.*)*', redirect: '/not-found' }
     ]
